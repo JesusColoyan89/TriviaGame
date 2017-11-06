61cc27398ff3a17f111d2ae19a7dbf83f5ce7f63
Jesus Coloyan <jesuscoloyan@jesuss-air.attlocal.net>
2017-11-05 22:12:04 -0800

    ran into a problem where it wouldn't let me add more than 3 questions, kept saying unexpected identifier even though everything was identical

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,203 +1,139 @@
+$("#start").on("click", function(){
+	game.start();
+})
 
-generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
-function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+$(document).on("click", '#end', function() {
+	game.done();
+})
 
-	function showQuestions(questions, quizContainer){
+var questions =[{
+	question:"Who recorded an NBA record, 30 assists in one game?",
+	answers:["Magic Johnson","Scott Skiles","John Stockton","Jason 'White Chocolate' Williams"],
+	correctAnswer:"Scott Skiles"
+}, {
+	question:"Who recorded an NBA record, 55 rebounds in one game?",
+	answers:["Shaquille Oneal","Bill Russell","Wilt Chamberlain","Dennis Rodman"],
+	correctAnswer:"Wilt Chamberlain"
+}, {
+	question:"Which player has the record for most Championship rings with 11?",
+	answers:["Michael Jordan","Larry Bird","Smush Parker","Bill Russell"],
+	correctAnswer:"Bill Russell"
 
+}];
 
-	var output = [];
-	var answers;
-
-
-	for(var i=0; i<questions.length; i++){
-			
-	
-		answers = [];
-
-		
-		for(letter in questions[i].answers){
-
-			
-			answers.push(
-				'<label>'
-					+ '<input type="radio" name="question'+i+'" value="'+letter+'">'
-					+ letter + ': '
-					+ questions[i].answers[letter]
-				+ '</label>'
-			);
+var game = {
+	correct: 0,
+	incorrect: 0,
+	counter: 20,
+	countdown: function() {
+		game.counter--;
+		$("#counter").html(game.counter);
+		if(game.counter<=0){
+			console.log("Time is up!");
+			game.done();
 		}
-
-		
-		output.push(
-			'<div class="question">' + questions[i].question + '</div>'
-			+ '<div class="answers">' + answers.join('') + '</div>'
-		);
-	}
-
+	},
 	
-	quizContainer.innerHTML = output.join('');
+	start: function(){
+		timer = setInterval(game.countdown,1000);
+		$('#subwrapper').prepend('<h2>Time Remaining: <span id="counter">120</span> Seconds</h2>');
+		$("#start").remove();
+		for(var i=0; i<questions.length; i++){
+			$('#subwrapper').append('<h2>'+questions[i].question+'</h2>');
+			for(var k=0; k<questions[i].answers.length;k++){
+				$("#subwrapper").append("<input type='radio' name='question-"+i+"'value='"+questions[i].answers[k]+"'>"+questions[i].answers[k])
+
+			}
+		}
+		$('#subwrapper').append('<button id="end">Done</button>')
+
+	},
+	done: function() {
+		$.each($('input[name="question-0]":checked'), function(){
+			if($(this).val()==questions[0].correctAnswer){
+				game.correct++;
+			} else {
+				game.incorrect++;
+			}
+		});
+		$.each($('input[name="question-1]":checked'), function(){
+			if($(this).val()==questions[1].correctAnswer){
+				game.correct++;
+			} else {
+				game.incorrect++;
+			}
+		});
+		$.each($('input[name="question-2]":checked'), function(){
+			if($(this).val()==questions[2].correctAnswer){
+				game.correct++;
+			} else {
+				game.incorrect++;
+			}
+		});
+		$.each($('input[name="question-3]":checked'), function(){
+			if($(this).val()==questions[3].correctAnswer){
+				game.correct++;
+			} else {
+				game.incorrect++;
+			}
+		});
+		$.each($('input[name="question-4]":checked'), function(){
+			if($(this).val()==questions[4].correctAnswer){
+				game.correct++;
+			} else {
+				game.incorrect++;
+			}
+		});
+		$.each($('input[name="question-5]":checked'), function(){
+			if($(this).val()==questions[5].correctAnswer){
+				game.correct++;
+			} else {
+				game.incorrect++;
+			}
+		});
+		$.each($('input[name="question-6]":checked'), function(){
+			if($(this).val()==questions[6].correctAnswer){
+				game.correct++;
+			} else {
+				game.incorrect++;
+			}
+		});
+		$.each($('input[name="question-7]":checked'), function(){
+			if($(this).val()==questions[7].correctAnswer){
+				game.correct++;
+			} else {
+				game.incorrect++;
+			}
+		});
+		$.each($('input[name="question-8]":checked'), function(){
+			if($(this).val()==questions[8].correctAnswer){
+				game.correct++;
+			} else {
+				game.incorrect++;
+			}
+		});
+		$.each($('input[name="question-9]":checked'), function(){
+			if($(this).val()==questions[9].correctAnswer){
+				game.correct++;
+			} else {
+				game.incorrect++;
+			}
+		});
+
+		this.result();
+
+	},
+	result: function(){
+		clearInterval(timer);
+		$("#subwrapper h2").remove();
+
+		$("#subwrapper").html("<h2>Youre Finished</h2>");
+		$("#subwrapper").append("<h3>Correct Answers: "+this.correct+"</h3>");
+		$("#subwrapper").append("<h3>Incorrect Answers: "+this.incorrect+"</h3>");
+		$("#subwrapper").append("<h3>Unanswered: "+(questions.length-(this.incorrect+this.correct))+"</h3>");
+	}	
+
 }
 
 
 
-		
-	}
-
-	function showResults(questions, quizContainer, resultsContainer){
-		var answerContainers = quizContainer.querySelectorAll('.answers');
-	
-	
-	var userAnswer = '';
-	var numCorrect = 0;
-	
-	
-	for(var i=0; i<questions.length; i++){
-
-		
-		userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
-		
-	
-		if(userAnswer===questions[i].correctAnswer){
-			
-			numCorrect++;
-			
-		
-			
-		}
-		
-	}
-
-	
-	resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
-
-
-
-		
-	}
-
-	
-	showQuestions(questions, quizContainer);
-
-	submitButton.onclick = function(){
-		showResults(questions, quizContainer, resultsContainer);
-	}
-
-
-
-var myQuestions = [
-	{
-	q1: "Who recorded an NBA record, 30 assists in one game?",
-	answers: {
-		a: "Magic Johnson",
-		b: "Scott Skiles",
-		c: "John Stockton",
-		d: "Jason 'White Chocolate' Williams"
-	},
-	correctAnswer: "b"
-},
-{
- 	q2: "Who recorded an NBA record, 55 rebounds in one game?",
- 	answers: {
- 		a: "Shaquille Oneal",
- 		b: "Bill Russell",
- 		c: "Wilt Chamberlain",
- 		d: "Dennis Rodman"
-
- 	},
- 	correctAnswer: "c"
- },	
-
-{
-	q3: "Which player has the record for most Championship rings with 11?",
-	answers: {
-		a: "Michael Jordan",
-		b: "Larry Bird",
-		c: "Smush Parker",
-		d: "Bill Russell"
-	
-	},
-	correctAnswer: "d"
-},
-{
-
-	q4: "Who has been assessed the most technical fouls?",
-	answers: {
-		a: "Rasheed Wallace",
-		b: "Bill Laimbeer",
-		c: "Karl Malone",
-		d: "Dwight Coward"
-	},
-	correctAnswer: "c"
-},
-{
-	q5: "Who has the most career points in NBA history?",
-	answers: {
-		a: "Kareem Abdul Jabbar",
-		b: "Michael Jordan",
-		c: "Kobe Bryant",
-		d: "Karl Malone"
-	},
-	correctAnswer: "a"
-},
-{
-
-	q6: "What is Kobe Bryant's middle name?",
-	answers: {
-		a: "Beef",
-		b: "Bean",
-		c: "Ball",
-		d: "Brian"
-	},
-
-	correctAnswer: "b"
-},
-{	
-
-	q7: "Which one of these names is actually an NBA player?",
-	answers: {
-		a: "God Shamgod",
-		b: "Jihad Muhammad",
-		c: "Crusader Christ",
-		d: "Batman Bradley"
-	},
-	correctAnswer: "a"
-},
-{
-
-
-	q8: "Standing at 5'3 this man was the shortest player in NBA history",
-	answers: {
-		a: "Spud Webb",
-		b: "Earl Boykins",
-		c: "Muggsy Bogues",
-		d: "Wataru Misaka"
-	},
-	correctAnswer: "c"
-},
-{
-
-	q9: "Standing at 7'7 this man was the tallest player in NBA history",
-	answers: {
-		a: "Mark Eaton",
-		b: "Manute Bol",
-		c: "Yao Ming",
-		d: "Shawn Bradley"
-	},
-	correctAnswer: "b"
-},
-
-{
-	q10: "Weighing in at an astounding 375 lbs this man was the heaviest NBA player in history",
-	answers: {
-		a: "Oliver Miller",
-		b: "Shaquille Oneal",
-		c: "Eddy Curry",
-		d: "Charles Barkley"
-
-	},
-	correctAnswer: "a"
-}
-
-var quizContainer = document.getElementById('quiz');
-var resultsContainer = document.getElementById('results');
-var submitButton = document.getElementById('submit');
